c4fdd86acc4605d417cb3f858c08df194a5981f6
antonialucianapires <antonialucianapires@gmail.com>
2024-12-11 09:43:29 -0300

    feat: conectando com o banco de dados

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/antonialucianapires/api-rest-crud-golang/database"
 	"github.com/antonialucianapires/api-rest-crud-golang/models"
 	"github.com/antonialucianapires/api-rest-crud-golang/routes"
 )
@@ -11,5 +12,6 @@ func main() {
 		{Id: 1, Name: "Ada Lovelace", History: "Pioneira da programação e autora do primeiro algoritmo."},
         {Id: 2, Name: "Alan Turing", History: "Fundador da ciência da computação e da criptoanálise moderna."},
 	}
+	database.ConnectDatabase()
 	routes.HandleRequest()
 }
